41873de9028b2defcf88736540a5645c2f7693ea
Edgar Hernandez <Eedgar.HMarc@gmail.com>
2022-05-28 23:01:40 -0500

    Se carga el primer componente, se realiza la carga de imagen

--- front-history-commits/src/App.jsx
+++ front-history-commits/src/App.jsx
@@ -1,30 +1,43 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import Perfil from "./components/Perfil";
+import Historial from "./components/Historial";
 
 
 function App() {
 
-  const [commits, setCommits] = useState({})
-
+  // const [commits, setCommits] = useState({})
+  
+  // const [photo, setPhoto] = useState('')
+/*
   useEffect( ()=>{
-    const getApi =async () => {
+    const getApi = async () => {
       const urlApi = "http://localhost:1337/api/getcommits";
       const resp = await axios(urlApi);
-      setCommits(resp)
+      setCommits(resp.data.response)
     }
   
     console.log(commits);
     getApi()
   },[])
+*/
 
+// useEffect( ()=>{
+//   const getApi = async () => {
+//     const urlApi = 'http://localhost:1337/api/getUserInfo'
+//     const resp = await axios(urlApi);
+//     console.log(resp);
+//     setPhoto(resp.data.response.avatar_url)
+//   }
+
+//   getApi()
+// },[])
 
   return (
-    <div>
-      <Perfil/>    
-      {
-        console.log(commits)
-      }  
+    <div className="grid grid-cols-2 gap-4">
+      
+      <Perfil/>
+      <Historial/>
     </div>
   )
 }
--- front-history-commits/src/components/Perfil.jsx
+++ front-history-commits/src/components/Perfil.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const Perfil = () => {
+
+    const [photo, setPhoto] = useState('')
+    const [info, setInfo] = useState({
+        avatar_url : '',
+        company:'',
+        followers:'',
+        name:''
+    })
+    useEffect( ()=>{
+      const getApi = async () => {
+        const urlApi = 'http://localhost:1337/api/getUserInfo'
+        const resp = await axios(urlApi);
+        setInfo({
+            avatar_url : resp.data.response.avatar_url,
+            company:resp.data.response.company,
+            followers:resp.data.response.followers,
+            name:resp.data.response.name
+        });
+      }
+
+    
+      getApi()
+    },[])
+    
   return (
-    <div>Perfil</div>
+    <div className='p-10'>
+        <img src={info.avatar_url} className='border border-slate-800 rounded-full shadow-md p-5 ' />
+        <p>{info.name}</p>
+    </div>
   )
 }
 
